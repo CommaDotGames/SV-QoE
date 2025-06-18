08952efec8fd826574df5c0c9c16f442ad56080d
AfshinDr50 <afshindr50@yahoo.com>
2025-06-18 10:35:23 +0200

    update 1

--- load-components.js
+++ load-components.js
@@ -1,1 +1,13 @@
-/*for test*/
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll('[data-include]').forEach(el => {
+    fetch(el.getAttribute('data-include'))
+      .then(response => response.text())
+      .then(data => {
+        el.innerHTML = data;
+      })
+      .catch(err => {
+        el.innerHTML = "<p style='color:red;'>Component failed to load.</p>";
+        console.error(err);
+      });
+  });
+});
